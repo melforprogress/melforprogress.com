b7e557fa32c8824b09a4c126804fa1005e287d76
Kathy Lu <kaffy.lu@gmail.com>
2019-07-28 00:11:24 -0400

    Add favicon (#12)

--- src/components/Billboard.js
+++ src/components/Billboard.js
@@ -5,6 +5,11 @@ import PageUnderNavbar from './PageUnderNavBar'
 import styles from './Billboard.module.css'
 import ActionNetworkWidget from './ActionNetworkWidget';
 
+// TODO:
+// This component is called "Billboard"
+// but it's actually the entire "Home" page
+// and should be renamed accordingly when we have a chance
+// (i.e., when there won't be merge conflicts.)
 export default function ({children}) {
   return (
     <div className={styles.billboard}>
