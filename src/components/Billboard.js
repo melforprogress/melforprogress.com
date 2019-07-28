3287d098d635f9e85f11129400dd59dc0c7d721f
warsucks <kathy@controlshiftlabs.com>
2019-07-27 23:25:57 -0400

    more cleanup

--- src/components/Billboard.js
+++ src/components/Billboard.js
@@ -5,29 +5,19 @@ import PageUnderNavbar from './PageUnderNavBar'
 import styles from './Billboard.module.css'
 import ActionNetworkWidget from './ActionNetworkWidget';
 
-export default class Billboard extends React.Component {
-  handleFormLoaded(){
-    setTimeout(
-      () => {
-        document.querySelector("#can-form-area-join-our-campaign-for-progress-2").classList.add("loaded")
-      },
-      100
-    );
-  }
-  render(){
-    return (
-      <div className={styles.billboard}>
-        <PageUnderNavbar>
-          <img id="headshot" src={headshot} />
-          {this.props.children}
-          <div className={styles.actionNetworkSection}>
-            <div style={{textAlign: 'center'}} className="title">Join Our Campaign for Progress!</div>
-          </div>
+export default function ({children}) {
+  return (
+    <div className={styles.billboard}>
+      <PageUnderNavbar>
+        <img id="headshot" src={headshot} />
+        {children}
+        <div className={styles.actionNetworkSection}>
+          <div style={{textAlign: 'center'}} className="title">Join Our Campaign for Progress!</div>
           <ActionNetworkWidget />
-        </PageUnderNavbar>
-      </div>
-    )
-  }
+        </div>
+      </PageUnderNavbar>
+    </div>
+  )
 }
 
 
