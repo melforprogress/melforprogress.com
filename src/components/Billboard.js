098671e24daf26ae23c8cc8f52274078ec00629e
Kathy Lu <kaffy.lu@gmail.com>
2019-07-27 23:29:43 -0400

    Separate ActionNetwork widget into its own component (#11)

--- src/components/Billboard.js
+++ src/components/Billboard.js
@@ -1,43 +1,23 @@
 import React from 'react'
-import Script from 'react-load-script'
 import headshot from '../img/content/supercropped.jpg'
 import PageUnderNavbar from './PageUnderNavBar'
 
 import styles from './Billboard.module.css'
+import ActionNetworkWidget from './ActionNetworkWidget';
 
-export default class Billboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {hashActionNetwork: Math.random() * 1000 };
-  }
-
-  handleFormLoaded(){
-    setTimeout(
-      () => {
-        document.querySelector("#can-form-area-join-our-campaign-for-progress-2").classList.add("loaded")
-      },
-      100
-    );
-  }
-  render(){
-    return (
-      <div className={styles.billboard}>
-        {/* add random number to script URL cos otherwise the widget won't be inserted if this page is loaded a second time */}
-        <Script
-          url={`https://actionnetwork.org/widgets/v3/form/join-our-campaign-for-progress-2?format=js&source=widget&hash=${this.state.hashActionNetwork}`}
-          onLoad={this.handleFormLoaded.bind(this)}
-        />
-        <PageUnderNavbar>
-          <img id="headshot" src={headshot} />
-          {this.props.children}
-          <div className={styles.actionNetworkSection}>
-            <div style={{textAlign: 'center'}} className="title">Join Our Campaign for Progress!</div>
-          </div>
-          <div id='can-form-area-join-our-campaign-for-progress-2'></div>
-        </PageUnderNavbar>
-      </div>
-    )
-  }
+export default function ({children}) {
+  return (
+    <div className={styles.billboard}>
+      <PageUnderNavbar>
+        <img id="headshot" src={headshot} />
+        {children}
+        <div className={styles.actionNetworkSection}>
+          <div style={{textAlign: 'center'}} className="title">Join Our Campaign for Progress!</div>
+          <ActionNetworkWidget />
+        </div>
+      </PageUnderNavbar>
+    </div>
+  )
 }
 
 
