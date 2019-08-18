ba0f3778be28f7b0505fe9dd480d19323ed854d8
garyhe <wow@wow.com>
2019-08-18 15:12:23 -0400

    Fix ActionNetwork form disappearing in mobile

--- src/components/PageUnderNavBar.js
+++ src/components/PageUnderNavBar.js
@@ -4,12 +4,8 @@ import styles from './PageUnderNavbar.module.css'
 
 export default function(props){
   return <div>
-    <div className={styles.pageUnderNavbarDesktop}>
-      {props.children}
-    </div>
-    <div className={styles.pageUnderNavbarMobile}>
+    <div className={styles.pageUnderNavbar}>
       {props.children}
     </div>
   </div>
-
 }
