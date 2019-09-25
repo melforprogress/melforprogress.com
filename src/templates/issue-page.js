8caf9a2eeecd1247c6df1ea346acba97f95a27c5
Eric Doctor <ericbdoctor@gmail.com>
2019-09-24 20:59:04 -0400

    Update issue-page.js
    Revised: Housing, Immigration, Elder Care, UBI; Added: Medicare for All, Green New Deal, Public Education, Public Banking, Criminal Justice Veterans, LGBTQIA

--- src/templates/issue-page.js
+++ src/templates/issue-page.js
@@ -31,48 +31,48 @@ const issuesDefaults = [
   {
     title: 'Immigration Justice',
     text: [
-      oneLine(`The Trump administration has taken pride in policies that rip away children
-        from their families, deprive our undocumented friends and neighbors of due process and
-        dignity, and erode any accountability of immigration agencies. It’s time to shut down
-        detention centers and terminate the private prison contracts that profit off them. We must
-        abolish ICE, end the militarization of the CBP, and develop sensible policy that does not
-        restrict movement while preserving safety.`),
-      oneLine(`Mel understands that we need to act against the atrocities being committed in our
-        country. Asylum seekers should be fast-tracked and given the chance to build the better life
-        that they came here seeking. America should be a beacon of hope, not a stage for cruel and
-        inhumane acts against refugees and immigrants.`),
-      oneLine(`By reforming our immigration infrastructure, Mel believes that we can keep
-        families together, build safer communities, and restore America’s promise as a land of
-        refuge and opportunity.`)
+      oneLine(`The Trump administration has taken pride in policies that rip away children from their families, deprive our undocumented friends and neighbors of due process and dignity, and erode any accountability of immigration agencies. It’s time to shut down detention centers and terminate all of the private prison contracts that profit off them. We must abolish ICE, end the militarization of the CBP, and develop sensible policy when it comes to our borders.`),
+      oneLine(`Mel understands that we need to act against the atrocities being committed in our country. Asylum seekers should be fast-tracked and given the chance to build the better life that they came here seeking. America should be a beacon of hope, not a stage for cruel and inhumane acts against refugees and immigrants.`),
+      oneLine(`Undocumented immigrants who were brought here as children only know America as their home. These children have grown up in America and are woven into the very fabric of our community. Mel will fight to secure DACA and DAPA, creating a path to citizenship for our DREAMer friends and neighbors. Mel is the descendant of immigrants who understands that America can and must do better for families.`),
+	  oneLine(`It’s time for America to value the work and contributions of immigrants.  By reforming our immigration infrastructure, Mel believes that we can keep families together, build safer communities, and restore America’s promise as a land of refuge and opportunity.`),
     ]
   },
   {
-    title: 'Universal Basic Income',
+    title: 'Public Banking',
     text: [
-      oneLine(`Rapid technological innovation has already changed American employment. Automation
-        continues to eviscerate opportunities for work and is contributing to an already-growing gig
-        economy. As temporary, part-time, and non-traditional jobs redefine the meaning of career,
-        it’s time to be prepared for the future.`),
-      oneLine(`Mel understands that we need to enable people to live better lives, not leave them
-        to struggle with wage stagnation and income inequality. Investing in Universal Basic Income
-        experiments can reduce poverty, create opportunities for new types of work, and reinforce a
-        sense of dignity in people’s lives.`),
+      oneLine(`City and state governments currently manage their finances with privately-owned banks, giving Wall Street institutions the power to speculate with public dollars, to lend money to municipalities at high interest rates, and to charge large fees for banking services. This is no surprise as private banks have a responsibility to aggressively maximize short-term profits, not invest in the best interests of the community. We need an alternative financial institution to put people before profit: public banks.`),
+      oneLine(`Public banks will answer to the public — not shareholder. By shifting the priority from shareholders to the public, public banks can both manage government finances and prioritize the needs of our communities and the public good.`),
+      oneLine(`Mel will fight for public banking because low-interest capital is necessary for a just and equitable economy. In order to thrive in communities, small business owners need access to low-interest loan alternatives — not be held hostage by outrageous rates of private loans.`),
+	  oneLine(`As a supporter of the Green New Deal, Mel believes public banking is a crucial component of decarbonizing the economy, building green infrastructure projects, and investing in clean energy alternatives. The bottom line is this: so long as the private banks have investments in industries like oil, they have no incentive to put dollars in sustainable, long-term investments into our community or the Green New Deal.`),
     ]
   },
   {
-    title: 'Support Seniors',
+    title: 'Criminal Justice Reform',
     text: [
-      oneLine(`For many Americans, the ability to enjoy retirement has become increasingly
-        difficult with rising costs of housing and healthcare. Mel believes that everyone
-        deserves to live out their golden years in dignity. Seniors who are retired should not
-        be priced out of their homes or unable to afford long-term quality healthcare, which is
-        why Mel also supports Medicare for All.`),
-      oneLine(`Mel will introduce the Golden Years Security Act to make sure that the specialized
-        needs of our elders are met, from preventing discrimination of LGBTQIA seniors in nursing
-        facilities to addressing the urgent medical needs that our current infrastructure fails to.
-        Whether subsidizing home care services, meeting the challenges of end-of-life care, or
-        improving the support of assisted living facilities, Mel believes our seniors are owed
-        respect and dignity in their golden years, regardless of financial circumstances.`),
+      oneLine(`There is no doubt that we have a broken criminal justice system, with black and brown communities disproportionately represented in the system. America incarcerates nearly 2.5 million individuals, the most of any developed nation. Mel will work to address our nation’s mass incarceration crisis and fight for legislation to address the root causes that fuels the system.`),
+      oneLine(`Mel is proud to have worked alongside impacted individuals fighting to eliminate barriers to higher education for currently and formerly incarcerated people. Mel knows that those closest to the problem are closest to the solution and their experiences help form his views on criminal justice reform.  The coalition that Mel worked with succeeded in seeing the introduction of the REAL Act in Congress, which would restore Pell grant access to incarcerated students (the coalition also worked with the Obama DOE which launched the Pell Pilot Program).  However, that bill still languishes in Congress despite studies that show access to higher education while incarcerated leads to lower recidivism rates and improved outcomes for impacted individuals and their families. Mel will make it a priority to fight for its passage to restore the harm done by the 1994 Crime Bill.`),
+      oneLine(`Mel believes ending cash bail must be a national priority to ensure that those that have not even been convicted of a crime are not further punished simply for being poor. Much needed reforms must be instituted to end cash bail without furthering the surveillance of individuals while awaiting trial.`),
+	  oneLine(`The rise in the incarceration rates of women are often left out of the national conversation or absorbed into the general conversation. However, the rate of incarceration for women has actually been increasing nationwide. In New York, while the rate for women has been slowly declining, it has not kept pace with the reduction of rates for men. Many women who find themselves in the system are victims of domestic abuse and prison furthers that trauma. Our jails and prisons are not mental health or counseling facilities. Mel believes the federal government must invest in diversion programs for all, but especially for women who have fewer options available to them, and that they are provided with appropriate trauma informed services.`),
+    ]
+  },
+  {
+    title: 'Repealing SESTA-FOSTA',
+    text: [
+      oneLine(`Another priority for Mel is the repeal of SESTA-FOSTA. This law was intended to address human trafficking, but in practice, has only made the situation worse by driving sex workers into the shadows while making it more difficult to combat legitimate traffickers. Sex work is work.`),
+      oneLine(`Mel believes that we need to decriminalize (as opposed to legalize) sex work which has led to the disproportionate targeting and prosecution of migrant workers and those in the trans community. Repealing SESTA-FOSTA and bringing sex workers into the policymaking conversation will lead to effective human trafficking legislation that is informed by those closest to the problem, while ensuring that those who rely on selling or trading sex are not prosecuted for simply trying to survive.`),
+    ]
+  },
+  {
+    title: 'Veterans',
+    text: [
+      oneLine(`As a Congressional aide, Mel worked closely with former service members as a liaison to the Department of Veteran’s Affairs. While Mel believes we need to end the forever wars, and develop foreign policy that puts mutual collaboration and cooperation at the fore, he also understands that our nation still owes a debt to those who have and continue to serve in wars they had no choice in entering. Billions are spent on Defense each fiscal year, but those dollars find themselves in the pockets of defense contractors and private corporations that make up the military industrial complex.`),
+      oneLine(`Mel will fight to ensure that the VA is properly funded, reverse legislation that makes it easier to fire VA employees, and strengthen programming that does outreach in order to provide mental health services to servicemembers. Mel will also fight to legislate permanent protections for members of the LGBTQIA community who wish to serve.`),
+    ]
+  },
+  {
+    title: 'LGBTQIA Rights',
+    text: [
+      oneLine(`Securing the rights of our LGBTQIA neighbors is vital, and isn’t a single, stand-alone issue. The LGBTQIA community is affected by all the issues addressed above, and that’s why you’ll see specific policies protecting LGBTQIA folks in issues like elder care, veterans, and repealing SESTA-FOSTA. The current administration’s assault on the LGBTQIA community is unacceptable. LGBTQIA people are still forced to fight for their rights in their towns, in congress, and in their day to day lives because they lack basic legal protections in states across the country. Federal law must provide consistent non-discrimination protections based on sexual orientation and gender identity. Mel will work tirelessly to ensure equal rights under the protection of the law to every member of this community. Mel supports the Equality Act which would provide consistent non-discrimination protections for LGBTQ people across key areas of life including education, credit, housing, public spaces, and more. He will be a champion for LGBTQIA rights, because for any of us to be truly free, we all must be. `),
     ]
   },
 ];
