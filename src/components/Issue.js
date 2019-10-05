f931f8e9da6216e15a77df7466512a945ad15e00
Doc <eric@ericdoctor.com>
2019-10-05 14:54:23 -0400

    Adding images to issues page

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import desktop_logo from '../img/content/mel-logo-web-desktop.png'
 
 export default function(props) {
   return (
     <div className="issue">
       <h3>{props.title}</h3>
+       <img className="desktop" src={desktop_logo} />
       {props.paragraphs.map(p => {
         return <p>{p}</p>
       })}
