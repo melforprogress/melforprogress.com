a454d566298ae33926b837461e91f26cace87c3e
Doc <eric@ericdoctor.com>
2019-10-19 17:08:04 -0400

    Links to Issues Page
    Adding links to issues page — did it on the wrong branch :/

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function(props) {
   return (
-    <div className="issue">
+    <div className="issue" id={props.id}>
        <img className="issue-bug" src={props.bugurl} aria-label={props.title} />
       {props.paragraphs.map(p => {
         return <p>{p}</p>
