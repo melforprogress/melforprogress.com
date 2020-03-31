59417b3e3afe3e706e0492e74363c6918d05c480
Kyle Levenick <klevenick@gmail.com>
2020-03-30 21:42:42 -0400

    Adding fetch

--- src/components/Resource.js
+++ src/components/Resource.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+const fetch = require("node-fetch");
+
 let iframeSRC = "https://docs.google.com/document/d/e/2PACX-1vTxA5mfzuOFOGbqCsNJnTCD5GjgAGfyd3sU3X3Lwev9oEb0Asnr0eHIRe8ytTxwqEf9kxH8J8rk1x_B/pub";
 var docDiv;
 var cleanResult = (data) => {
