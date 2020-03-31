cfea6b71290f2d88f51d38fe24f17bbe0128c337
Kyle Levenick <klevenick@gmail.com>
2020-03-31 12:45:01 -0400

    Adding debug

--- src/components/Resource.js
+++ src/components/Resource.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 let iframeSRC = "https://docs.google.com/document/d/e/2PACX-1vTxA5mfzuOFOGbqCsNJnTCD5GjgAGfyd3sU3X3Lwev9oEb0Asnr0eHIRe8ytTxwqEf9kxH8J8rk1x_B/pub";
 var docDiv;
 var cleanResult = (data) => {
+  console.log("We have data!");
   console.log(data);
   docDiv = data;
   docDiv = docDiv.slice(docDiv.indexOf("<body>")+6,docDiv.indexOf('<div id="footer">'));
