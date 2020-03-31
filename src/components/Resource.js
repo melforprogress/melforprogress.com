68bf552e65788122ea716fa05d36312638b68880
Kyle Levenick <klevenick@gmail.com>
2020-03-30 21:33:23 -0400

    using fetch instead of xhr, styling changes

--- src/components/Resource.js
+++ src/components/Resource.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
 let iframeSRC = "https://docs.google.com/document/d/e/2PACX-1vTxA5mfzuOFOGbqCsNJnTCD5GjgAGfyd3sU3X3Lwev9oEb0Asnr0eHIRe8ytTxwqEf9kxH8J8rk1x_B/pub";
-
-// create div and replace iframe
 var docDiv;
-// CORS request
-var xhr = new XMLHttpRequest();
-xhr.open('GET', iframeSRC, true);
-xhr.onload = function() {
-    // display response
-   docDiv = xhr.responseText;
-   docDiv = docDiv.slice(docDiv.indexOf("<body>")+6,docDiv.indexOf('<div id="footer">'));
-   docDiv = docDiv.slice(docDiv.indexOf("</style>")+8);
-};
-xhr.send();
+var cleanResult = (data) => {
+  console.log(data);
+  docDiv = data;
+  docDiv = docDiv.slice(docDiv.indexOf("<body>")+6,docDiv.indexOf('<div id="footer">'));
+  docDiv = docDiv.slice(docDiv.indexOf("</style>")+8);
+}
+
+fetch(iframeSRC)
+  .then((response) => response.text())
+  .then((data) => 
+        cleanResult(data)
+        );
+
+
 
 export default function(props) {
   return (
