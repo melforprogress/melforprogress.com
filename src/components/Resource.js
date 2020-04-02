3db287393b56e7958e645e32093a1fda27e19a44
Kyle Levenick <klevenick@gmail.com>
2020-04-02 16:19:43 -0400

    Cleanup on resource

--- src/components/Resource.js
+++ src/components/Resource.js
@@ -4,31 +4,15 @@ const axios = require('axios');
 
 let iframeSRC = "https://docs.google.com/document/d/e/2PACX-1vTxA5mfzuOFOGbqCsNJnTCD5GjgAGfyd3sU3X3Lwev9oEb0Asnr0eHIRe8ytTxwqEf9kxH8J8rk1x_B/pub";
 let docDiv;
-//var parseDoc;
 var cleanResult = (data) => {
   console.log("We have data!");
-  //console.log(data);
   docDiv = data;
   docDiv = docDiv.slice(docDiv.indexOf("<body>")+6,docDiv.indexOf('<div id="footer">'));
   docDiv = docDiv.slice(docDiv.indexOf("</style>")+8);
   docDiv = docDiv.replace(/(<p[^>]+?>|<p>|<\/p>)/img, "");
-  //console.log("Here is the sliced data:");
-  //console.log(docDiv)
-  //parseDoc = (new DOMParser().parseFromString(docDiv, "text/html"))
+ 
   return docDiv;
 }
-//var askReload = () => { docDiv = "<div> Please Reload the Page</div> "}
-
-/*axios.get(iframeSRC, { crossdomain: true })
-  .then((response) => 
-    cleanResult(response.data)
-  )
-  .catch((response) => 
-        askReload()
-        );*/
-
-
-
 
 export default function(props) {
 
@@ -36,13 +20,7 @@ export default function(props) {
   useEffect(() => {
     axios.get(iframeSRC, { crossdomain: true })
     .then((response) => setDocData(cleanResult(response.data)) )
-    //.catch((response) => askReload() );
-    // get data from GitHub api
-    /*fetch(`https://api.github.com/repos/gatsbyjs/gatsby`)
-      .then(response => response.json()) // parse JSON from request
-      .then(resultData => {
-        setStarsCount(resultData.stargazers_count)
-      }) // set data for the number of stars*/
+ 
   }, [])
 
   return (
